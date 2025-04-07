6dc642c951abe87d0e989469ef199bd7f914fd7a
Laurens <Laurens-Noordover@live.nmit.ac.nz>
2025-04-07 13:44:17 +1200

    Initial game board creation

---
+++ index.js
@@ -0,0 +1,5 @@
+let gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(`
+${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]} 
+${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]} 
+${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}`);
