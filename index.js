ae4f88ff75c87f966138c4ada46f4acad6f1d41f
Laurens <Laurens-Noordover@live.nmit.ac.nz>
2025-04-15 10:16:50 +1200

    GameBoard creation finished

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Sets a constant array which will be used as the gameboard
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+//Creates the gameboard using a template litiral. Calls the value which is stored in the gameboard array
+function gameBoardDisplay(){
 console.log(`
 ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]} 
 ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]} 
 ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}`);
+return;
+};
