aa30c85e9c94de8b637851ea1b433f15db70fa7b
Laurens <Laurens-Noordover@live.nmit.ac.nz>
2025-04-25 00:50:11 +1200

    Started making win condtions for game. Have the Array mage and included and alternitve condition that the first if statments see when the game is running. In this alternitve condtion it sees if there is a player who has got a line of 3 of the same letters which matches the winCondtions set out in the constant variable winCondtitions. Also added some text to show whos turn it is, the number of the cell they selected to put thier number in, and an error game if the program ever ends up going into the first if statement but the gameBoard is not full or if there are currently no winners.

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+//Variables used for player turns and storing numbers 
+let currentPlayer;
+let turn = 1;
+const xTurns = [1, 3, 5, 7, 9];
+let gameRunning = true;
+let x;
+let playerWon = false;
+
 //Sets a constant array which will be used as the gameboard
 const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //To see the generated x numbers
 const takenNumbers = [];
-
-let turn = 1;
-const xTurns = [1, 3, 5, 7, 9];
-let gameRunning = true;
-
+const winConditions = [ [0, 1, 2],
+                        [3, 4, 5],
+                        [6, 7, 8],
+                        [0, 3, 6],
+                        [1, 4, 7],
+                        [2, 5, 8],
+                        [0, 4, 8],
+                        [2, 4, 6]
+                      ];
 
 //Creates the gameboard using a template litiral. Calls the value which is stored in the gameboard array
 function gameBoardDisplay(){
 console.log(`
 ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]} 
 ${gameBoard[4]} | ${gameBoard[5]} | ${gameBoard[6]} 
-${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}`);
+${gameBoard[7]} | ${gameBoard[8]} | ${gameBoard[9]}
+\n \n`);
 return;
 };
 
@@ -21,23 +34,54 @@ return;
 while(gameRunning){
     //Creates a random number that is used to place the X in the gameboard array
     let i = Math.floor(Math.random() * 10);
+    for(let e = 0; i < winConditions.length; e++){
+        const condition = winConditions[e];
+        const cellA = gameBoard[condition [0]];
+        const cellB = gameBoard[condition [1]];
+        const cellC = gameBoard[condition [2]];
+        if(typeof cellA === 'number' ||  typeof cellB === 'number' ||  typeof cellC === 'number'){
+            continue
+        }
+        if(cellA == cellB && cellB == cellC){
+            playerWon = true;
+            continue
+        }
+    }
+    if(takenNumbers.length === 10 || playerWon == true){
+        gameRunning = false;;
         if(takenNumbers.length === 10){
-            gameRunning = false;;
-            console.log("Game done");
-            return;
-        } else if (takenNumbers.includes(i) == false){
-            if(xTurns.includes(turn) == true){
-                takenNumbers.push(i);
-                gameBoard[i] = "X";
-                gameBoardDisplay();
-                console.log(takenNumbers);
-                turn++;
-            }else{
-                takenNumbers.push(i);
-                gameBoard[i] = "O";
-                gameBoardDisplay();
-                console.log(takenNumbers);
-                turn++;
+            console.log("Game over no moves left, no winners :(");
+            gameBoardDisplay();
+        }
+        if(playerWon == true){
+            console.log(`Player ${ "winner" } wins! :D`);
+            gameBoardDisplay();
+        }
+        else{
+            console.log("Game error, please restart game");
+        }    
+    }
+    else if (takenNumbers.includes(i) == false){
+        if(xTurns.includes(turn) == true){
+            currentPlayer = "X";
+            console.log(`${ currentPlayer }'s turn. Please select an avalible number from the gameboard.`);
+            gameBoardDisplay();
+            console.log(`You have selected number ${ i }`);
+            takenNumbers.push(i);
+            gameBoard[i] = "X";
+            console.log(`The updated game board is the following`);
+            gameBoardDisplay();                
+            turn++;
+        }else{
+            currentPlayer = "O";
+            console.log(`${ currentPlayer }'s turn. Please select an avalible number from the gameboard.`);
+            gameBoardDisplay();
+            console.log(`You have selected number ${ i }`);
+            takenNumbers.push(i);
+            gameBoard[i] = "O";
+            console.log(`The updated game board is the following`);
+            gameBoardDisplay();
+            turn++;
             };
         };
     };
